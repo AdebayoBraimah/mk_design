4728082102be9a3f0e086e6572922cde1a91b306
AdebayoBraimah <adebayo.braimah@gmail.com>
2020-09-03 18:42:01 -0400

    Tidied up directory, removed extraneous test files

--- quick_test.sh
+++ quick_test.sh
@@ -98,73 +98,71 @@ for (( i=0; i < ${#out_dirs[@]}; i++ )); do
   mv ${out}/grp.design.test.all_info.txt ${out}/grp.design.test.all_info.csv
 done
 
-# Auxillary test for study design matrix
-parent_out_dir=${scripts_dir}/tests.auxillary 
-template_design=$(realpath jpy.notebooks/master.template.design.csv)
-
-out_dirs=( design_sct_no_covs
-  sct_adhd_parent_no_covs
-  sct_adhd_teacher_no_covs
-  sct_child_no_covs
-  sct_parent_no_covs
-  sct_teacher_no_covs
-  design_sct_covs
-  sct_adhd_parent_covs
-  sct_adhd_teacher_covs
-  sct_child_covs
-  sct_parent_covs
-  sct_teacher_covs 
-  sct_child_parent_adhd_covs 
-  sct_cci-2_parent_adhd_covs
-  sct_cci-2_covs )
-
-des_covs=( "3,4,5"
-  "6,7" 
-  "8,9" 
-  "10"
-  "6"
-  "8" 
-  "1,2,3,4,5" 
-  "1,2,6,7"
-  "1,2,8,9"
-  "1,2,10"
-  "1,2,6"
-  "1,2,8" 
-  "1,2,7,10" 
-  "1,2,7,11" 
-  "1,2,11" )
-
-if [[ ${#out_dirs[@]} != ${#des_covs[@]} ]] && [[ ${#out_dirs[@]} != ${#demean_cols[@]} ]]; then
-  echo ""
-  echo "Uneven length arrays. Exiting..."
-  # exit 1
-else
-  echo ""
-  echo "All input arrays are of the same length. Proceeding with tests."
-fi
-
-for (( i=0; i < ${#out_dirs[@]}; i++ )); do
-  # Print message
-  echo ""
-  echo "Running test: ${out_dirs[$i]}"
-
-  echo "" >> ${log}
-  echo "Running test: ${out_dirs[$i]}" >> ${log}
-
-  # Make output directory
-  out=${parent_out_dir}/${out_dirs[$i]}
-  if [[ ! -d ${out} ]]; then
-    mkdir -p ${out}
-  fi
-
-  # Run test
-  ${mk_design} --in=${template_design} --out=${out}/grp.design.test --rm-list="sub-1013,sub-1515,sub-1569" --ret-cols="${des_covs[$i]}" --demean="1" --sep="," >> ${log} 2>> ${log}
-
-  # rename output information file to csv for easy checking
-  mv ${out}/grp.design.test.all_info.txt ${out}/grp.design.test.all_info.csv
-done
-
-
+# # Auxillary test for study design matrix
+# parent_out_dir=${scripts_dir}/tests.auxillary 
+# template_design=$(realpath jpy.notebooks/master.template.design.csv)
+#
+# out_dirs=( design_sct_no_covs
+#   sct_adhd_parent_no_covs
+#   sct_adhd_teacher_no_covs
+#   sct_child_no_covs
+#   sct_parent_no_covs
+#   sct_teacher_no_covs
+#   design_sct_covs
+#   sct_adhd_parent_covs
+#   sct_adhd_teacher_covs
+#   sct_child_covs
+#   sct_parent_covs
+#   sct_teacher_covs 
+#   sct_child_parent_adhd_covs 
+#   sct_cci-2_parent_adhd_covs
+#   sct_cci-2_covs )
+#
+# des_covs=( "3,4,5"
+#   "6,7" 
+#   "8,9" 
+#   "10"
+#   "6"
+#   "8" 
+#   "1,2,3,4,5" 
+#   "1,2,6,7"
+#   "1,2,8,9"
+#   "1,2,10"
+#   "1,2,6"
+#   "1,2,8" 
+#   "1,2,7,10" 
+#   "1,2,7,11" 
+#   "1,2,11" )
+#
+# if [[ ${#out_dirs[@]} != ${#des_covs[@]} ]] && [[ ${#out_dirs[@]} != ${#demean_cols[@]} ]]; then
+#   echo ""
+#   echo "Uneven length arrays. Exiting..."
+#   # exit 1
+# else
+#   echo ""
+#   echo "All input arrays are of the same length. Proceeding with tests."
+# fi
+#
+# for (( i=0; i < ${#out_dirs[@]}; i++ )); do
+#   # Print message
+#   echo ""
+#   echo "Running test: ${out_dirs[$i]}"
+#
+#   echo "" >> ${log}
+#   echo "Running test: ${out_dirs[$i]}" >> ${log}
+#
+#   # Make output directory
+#   out=${parent_out_dir}/${out_dirs[$i]}
+#   if [[ ! -d ${out} ]]; then
+#     mkdir -p ${out}
+#   fi
+#
+#   # Run test
+#   ${mk_design} --in=${template_design} --out=${out}/grp.design.test --rm-list="sub-1013,sub-1515,sub-1569" --ret-cols="${des_covs[$i]}" --demean="1" --sep="," >> ${log} 2>> ${log}
+#
+#   # rename output information file to csv for easy checking
+#   mv ${out}/grp.design.test.all_info.txt ${out}/grp.design.test.all_info.csv
+# done
 
 echo ""
 echo "Done!"
