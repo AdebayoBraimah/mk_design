4cb5c0a1e6b97f54356b314842d0a64f92c8fe16
AdebayoBraimah <adebayo.braimah@gmail.com>
2020-09-05 02:48:51 -0400

    Wrote unit and integration tests

---
+++ tests/test.design.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+# Write some documentation
+parent_out_dir=${1}
+
+# Directory variables
+scripts_dir=$(dirname $(realpath ${0}))
+# parent_out_dir=${scripts_dir}/tests.results
+
+# File variables
+mk_design=${scripts_dir}/../mk_design.py
+log=${scripts_dir}/test.log
+err=${scripts_dir}/test.err
+
+# Arrays
+template_designs=( benchmark/design.test.csv
+  benchmark/design.test.csv
+  benchmark/design.test.csv
+  benchmark/design.test.csv )
+
+out_dirs=( grp_design
+  grp_design_no_covs
+  grp_design_demean_age
+  grp_design_demean_multi-col )
+
+des_covs=( "1,2,3,4,5,6"
+  "3,4"
+  "1,2,3,4,5,6"
+  "1,2,5,6" )
+
+demean_cols=( ""
+  ""
+  "1"
+  "1,3,4" )
+
+rm_subs=( "sub-009,sub-023,sub-039"
+  "sub-009,sub-023,sub-039"
+  "sub-009,sub-023,sub-039"
+  "sub-009,sub-023,sub-039" )
+
+if [[ ${#out_dirs[@]} != ${#des_covs[@]} ]] && [[ ${#out_dirs[@]} != ${#demean_cols[@]} ]]; then
+  echo ""
+  echo "Uneven length arrays. Exiting..."
+  # exit 1
+else
+  echo ""
+  echo "All input arrays are of the same length. Proceeding with tests."
+fi
+
+# Run each test
+for (( i=0; i < ${#out_dirs[@]}; i++ )); do
+  # Print message
+  echo ""
+  echo "Running test: ${out_dirs[$i]}"
+
+  echo "" >> ${log}
+  echo "Running test: ${out_dirs[$i]}" >> ${log}
+
+  # Make output directory
+  out=${parent_out_dir}/${out_dirs[$i]}
+  if [[ ! -d ${out} ]]; then
+    mkdir -p ${out}
+  fi
+
+  # Get abspath for template file
+  template_design=$(realpath ${template_designs[$i]})
+
+  # Run test
+  ${mk_design} --in=${template_design} --out=${out}/grp.design.test --rm-list="${rm_subs[$i]}" --ret-cols="${des_covs[$i]}" --demean="${demean_cols[$i]}" --sep="," >> ${log} 2>> ${log}
+
+  # rename output information file to csv for easy checking
+  # mv ${out}/grp.design.test.all_info.txt ${out}/grp.design.test.all_info.csv
+done
+
+# # Auxillary test for study design matrix
+# parent_out_dir=${scripts_dir}/tests.auxillary 
+# template_design=$(realpath master.template.design.csv)
+#
+# out_dirs=( design_sct_no_covs
+#   sct_adhd_parent_no_covs
+#   sct_adhd_teacher_no_covs
+#   sct_child_no_covs
+#   sct_parent_no_covs
+#   sct_teacher_no_covs
+#   design_sct_covs
+#   sct_adhd_parent_covs
+#   sct_adhd_teacher_covs
+#   sct_child_covs
+#   sct_parent_covs
+#   sct_teacher_covs 
+#   sct_child_parent_adhd_covs 
+#   sct_cci-2_parent_adhd_covs
+#   sct_cci-2_covs )
+#
+# des_covs=( "3,4,5"
+#   "6,7" 
+#   "8,9" 
+#   "10"
+#   "6"
+#   "8" 
+#   "1,2,3,4,5" 
+#   "1,2,6,7"
+#   "1,2,8,9"
+#   "1,2,10"
+#   "1,2,6"
+#   "1,2,8" 
+#   "1,2,7,10" 
+#   "1,2,7,11" 
+#   "1,2,11" )
+#
+# if [[ ${#out_dirs[@]} != ${#des_covs[@]} ]] && [[ ${#out_dirs[@]} != ${#demean_cols[@]} ]]; then
+#   echo ""
+#   echo "Uneven length arrays. Exiting..."
+#   # exit 1
+# else
+#   echo ""
+#   echo "All input arrays are of the same length. Proceeding with tests."
+# fi
+#
+# for (( i=0; i < ${#out_dirs[@]}; i++ )); do
+#   # Print message
+#   echo ""
+#   echo "Running test: ${out_dirs[$i]}"
+#
+#   echo "" >> ${log}
+#   echo "Running test: ${out_dirs[$i]}" >> ${log}
+#
+#   # Make output directory
+#   out=${parent_out_dir}/${out_dirs[$i]}
+#   if [[ ! -d ${out} ]]; then
+#     mkdir -p ${out}
+#   fi
+#
+#   # Run test
+#   ${mk_design} --in=${template_design} --out=${out}/grp.design.test --rm-list="sub-1013,sub-1515,sub-1569" --ret-cols="${des_covs[$i]}" --demean="1" --sep="," >> ${log} 2>> ${log}
+#
+#   # rename output information file to csv for easy checking
+#   mv ${out}/grp.design.test.all_info.txt ${out}/grp.design.test.all_info.csv
+# done
+
+echo ""
+echo "Done!"
+echo ""
---
+++ tests/tests.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+
+# Import modules/packages
+import pytest
+import subprocess
+import logging
+import os
+import shutil
+import sys
+import glob
+
+# Define global variables
+scripts_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Define class(es)
+class Command(object):
+    '''
+    Creates a command and an empty command list for UNIX command line programs/applications. Primary use and
+    use-cases are intended for the subprocess module and its associated classes (i.e. Popen/call/run).
+    
+    Attributes (class and instance attributes):
+        command (instance): Command to be performed on the command line.
+        cmd_list (instance): Mutable list that can be appended to.
+    
+    Modules/Packages required:
+        - os
+        - logging
+        - subprocess
+    '''
+
+    def __init__(self,command):
+        '''
+        Init doc-string for Command class. Initializes a command to be used on UNIX command line.
+        The input argument is a command (string), and a mutable list is returned (, that can later
+        be appended to).
+        
+        Usage:
+            echo = Command("echo")
+            echo.cmd_list.append("Hi!")
+            echo.cmd_list.append("I have arrived!")
+        
+        Arguments:
+            command (string): Command to be used. Note: command used must be in system path
+        Returns:
+            cmd_list (list): Mutable list that can be appended to.
+        '''
+        self.command = command
+        self.cmd_list = [f"{self.command}"]
+        
+    def log(self,log_file="log_file.log",log_cmd=""):
+        '''
+        Log function for logging commands and messages to some log file.
+        
+        Usage:
+            # Initialize the `log` function command
+            log_msg = Command("log")
+            
+            # Specify output file and message
+            log_msg.log("sub.log","test message 1")
+            
+            # Record message, however - no need to re-initialize `log` funcion command or log output file
+            log_msg.log("test message 2")
+        
+        NOTE: The input `log_file` only needs to be specified once. Once specified,
+            this log is written to each time this or the `run` function is invoked.
+        
+        Arguments:
+            log_file(file): Log file to be written to. 
+            log_cmd(str): Message to be written to log file
+        '''
+        
+        # Set-up logging to file
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                            datefmt='%d-%m-%y %H:%M:%S',
+                            filename=log_file,
+                            filemode='a')
+        
+        # Define a Handler which writes INFO messages or higher to the sys.stderr
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        
+        # Add the handler to the root logger
+        logging.getLogger().addHandler(console)
+        
+        # Define logging
+        logger = logging.getLogger(__name__)
+        
+        # Log command/message
+        logger.info(f"{log_cmd}")
+        
+    def run(self,log_file="log_file.log",debug=False,dryrun=False,env=None,stdout="",shell=False):
+        '''
+        Uses python's built-in subprocess class to execute (run) a command from an input command list.
+        The standard output and error can optionally be written to file.
+        
+        Usage:
+            echo.run() # This will return tuple (returncode,log,None,None), but will echo "Hi!" to screen.
+            
+        NOTE: 
+            - The contents of the 'stdout' output file will be empty if 'shell' is set to True.
+            - Once the log file name 'log_file' has been set, that value is stored and cannot be changed.
+                - This log file will continue to be appended to for each invocation of this class.
+        Arguments:
+            log_file(file): Output log file name.
+            debug(bool): Sets logging function verbosity to DEBUG level
+            dryrun(bool): Dry run -- does not run task. Command is recorded to log file.
+            env(dict): Dictionary of environment variables to add to subshell.
+            stdout(file): Output file to write standard output to.
+            shell(bool): Use shell to execute command.
+        Returns:
+            p.returncode(int): Return code for command execution should the 'log_file' option be used.
+            log_file(file): Output log file with appended information should the 'log_file' option be used.
+            stdout(file): Standard output writtent to file should the 'stdout' option be used.
+            stderr(file): Standard error writtent to file should the 'stdout' option be used.
+        '''
+        
+        # Define logging
+        logger = logging.getLogger(__name__)
+        cmd = ' '.join(self.cmd_list) # Join list for logging purposes
+        
+        if debug:
+            logger.debug(f"Running: {cmd}")
+        else:
+            logger.info(f"Running: {cmd}")
+        
+        if dryrun:
+            logger.info("Performing command as dryrun")
+            return 0
+        
+        # Define environment variables
+        merged_env = os.environ
+        if env:
+            merged_env.update(env)
+        
+        # Execute/run command
+        p = subprocess.Popen(self.cmd_list,shell=shell,env=merged_env,
+                        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+        # Write log files
+        out,err = p.communicate()
+        out = out.decode('utf-8')
+        err = err.decode('utf-8')
+
+        # Write std output/error files
+        if stdout:
+            stderr = os.path.splitext(stdout)[0] + ".err"
+            with open(stdout,"w") as f_out:
+                with open(stderr,"w") as f_err:
+                    f_out.write(out)
+                    f_err.write(err)
+                    f_out.close(); f_err.close()
+        else:
+            stdout = None
+            stderr = None
+
+        if p.returncode:
+            logger.error(f"command: {cmd} \n Failed with returncode {p.returncode}")
+
+        if len(out) > 0:
+            if debug:
+                logger.debug(out)
+            else:
+                logger.info(out)
+
+        if len(err) > 0:
+            if debug:
+                logger.info(err)
+            else:
+                logger.warning(err)
+        return p.returncode,log_file,stdout,stderr
+
+# Writes functions
+def write_design_files(out_dir):
+    '''
+    Wrapper function for `test.design.sh` script
+    NOTE: scripts_dir is assumed to be a global variable
+    '''
+    test = Command(os.path.join(scripts_dir,"test.design.sh"))
+    test.cmd_list.append(out_dir)
+    [returncode,log_file,stdout,stderr] = test.run(log_file="log_file.log",shell=False)
+    return returncode,log_file,stdout,stderr
+
+def compare_file(file1,file2):
+    '''
+    Compares two different files line-by-line
+    NOTE: The second file (file2) is the file in which differences 
+        should be identified.
+    '''
+    lines = []
+    with open(file1,'r') as f1:
+        with open(file2,'r') as f2:
+            for line1,line2 in zip(f1,f2):
+                if line1 != line2:
+                    lines = line2
+            f1.close(); f2.close()
+    return lines
+
+def directory_file_lists(bench_dir,test_dir):
+    '''
+    Inputs are top-level parent directories
+    '''
+    
+    bench_list = glob.glob(os.path.join(bench_dir,"*grp*/*"),recursive=False)
+    test_list = glob.glob(os.path.join(out_dir,"*grp*/*"),recursive=False)
+    return [bench_list,test_list]
+
+def design_matrices_test(out_dir,bench_dir="benchmark"):
+    '''
+    Tests if design files in each directory are the same
+    '''
+    [returncode,log_file,stdout,stderr] = write_design_files(out_dir)
+    if returncode != 0:
+        print("")
+        print("Non-zero error code writing test matrices")
+        sys.exit(18)
+    
+    [bench_files,test_files] = directory_file_lists("benchmark",out_dir)
+    
+    files = []
+    for f1,f2 in zip(bench_files,test_files):
+        lines = compare_file(f1,f2)
+        if len(lines) > 0:
+            tmp_list = [f2]
+            files.append(tmp_list)
+    
+    # Clean-up
+    os.remove("test.log")
+    shutil.rmtree(out_dir)
+    
+    if len(files) > 0:
+        print("Test failure. Output design matrices appear to be different")
+        return False
+    else:
+        print("Tests passed.")
+        return True
+
+def test_passed(out_dir):
+    assert design_matrices_test("benchmark",out_dir)
+
+if __name__ == "__main__":
+    out_dir = "test.results"
+    test_passed(out_dir)
